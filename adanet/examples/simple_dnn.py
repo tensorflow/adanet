d62879de8558b117f2a7b3357d664cefca0bbd80
Charles Weill <weill@google.com>
2018-08-09 17:36:34 -0400

    Create an example using a simple dense neural network search space.
    PiperOrigin-RevId: 208112877

---
+++ adanet/examples/simple_dnn.py
@@ -0,0 +1,213 @@
+"""A simple dense neural network search space.
+
+Copyright 2018 The AdaNet Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+
+import adanet
+import tensorflow as tf
+
+_NUM_LAYERS_KEY = "num_layers"
+
+
+class _SimpleDNNBuilder(adanet.BaseLearnerBuilder):
+  """Builds a DNN base learner for AdaNet."""
+
+  def __init__(self, feature_columns, optimizer, layer_size, num_layers,
+               learn_mixture_weights, dropout, seed):
+    """Initializes a `_DNNBuilder`.
+
+    Args:
+      feature_columns: An iterable containing all the feature columns used by
+        the model. All items in the set should be instances of classes derived
+        from `FeatureColumn`.
+      optimizer: An `Optimizer` instance for training both the base learner and
+        the mixture weights.
+      layer_size: The number of nodes to output at each hidden layer.
+      num_layers: The number of hidden layers.
+      learn_mixture_weights: Whether to solve a learning problem to find the
+        best mixture weights, or use their default value according to the
+        mixture weight type. When `False`, the base learners will return a no_op
+        for the mixture weight train op.
+      dropout: The dropout rate, between 0 and 1. E.g. "rate=0.1" would drop
+        out 10% of input units.
+      seed: A random seed.
+
+    Returns:
+      An instance of `_DNNBuilder`.
+    """
+
+    self._feature_columns = feature_columns
+    self._optimizer = optimizer
+    self._layer_size = layer_size
+    self._num_layers = num_layers
+    self._learn_mixture_weights = learn_mixture_weights
+    self._dropout = dropout
+    self._seed = seed
+
+  def build_base_learner(self,
+                         features,
+                         logits_dimension,
+                         training,
+                         iteration_step,
+                         summary,
+                         previous_ensemble=None):
+    """See `adanet.BaseLearnerBuilder`."""
+
+    input_layer = tf.feature_column.input_layer(
+        features=features, feature_columns=self._feature_columns)
+    last_layer = input_layer
+    for _ in range(self._num_layers):
+      last_layer = tf.layers.dense(
+          last_layer,
+          units=self._layer_size,
+          activation=tf.nn.relu,
+          kernel_initializer=tf.glorot_uniform_initializer(seed=self._seed))
+      last_layer = tf.layers.dropout(
+          last_layer, rate=self._dropout, seed=self._seed, training=training)
+    logits = tf.layers.dense(
+        last_layer,
+        units=logits_dimension,
+        kernel_initializer=tf.glorot_uniform_initializer(seed=self._seed))
+
+    # Approximate the Rademacher complexity of this base learner as the square-
+    # root of its depth.
+    complexity = tf.sqrt(tf.to_float(self._num_layers))
+
+    with tf.name_scope(""):
+      summary.scalar("complexity", complexity)
+      summary.scalar("num_layers", self._num_layers)
+
+    persisted_tensors = {_NUM_LAYERS_KEY: tf.constant(self._num_layers)}
+    return adanet.BaseLearner(
+        last_layer=last_layer,
+        logits=logits,
+        complexity=complexity,
+        persisted_tensors=persisted_tensors)
+
+  def build_base_learner_train_op(self, loss, var_list, labels, iteration_step,
+                                  summary):
+    """See `adanet.BaseLearnerBuilder`."""
+
+    # NOTE: The `adanet.Estimator` increments the global step.
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      return self._optimizer.minimize(loss=loss, var_list=var_list)
+
+  def build_mixture_weights_train_op(self, loss, var_list, logits, labels,
+                                     iteration_step, summary):
+    """See `adanet.BaseLearnerBuilder`."""
+
+    if not self._learn_mixture_weights:
+      return tf.no_op("mixture_weights_train_op")
+
+    # NOTE: The `adanet.Estimator` increments the global step.
+    return self._optimizer.minimize(loss=loss, var_list=var_list)
+
+  @property
+  def name(self):
+    """See `adanet.BaseLearnerBuilder`."""
+
+    if self._num_layers == 0:
+      # A DNN with no hidden layers is a linear model.
+      return "linear"
+    return "{}_layer_dnn".format(self._num_layers)
+
+
+class Generator(adanet.BaseLearnerBuilderGenerator):
+  """Generates a two DNN base learners at each iteration.
+
+  The first DNN has an identical shape to the most recently added base learner
+  in `previous_ensemble`. The second has the same shape plus one more dense
+  layer on top. This is similar to the adaptive network presented in Figure 2 of
+  [Cortes et al. ICML 2017](https://arxiv.org/abs/1607.01097), without the
+  connections to hidden layers of networks from previous iterations.
+  """
+
+  def __init__(self,
+               feature_columns,
+               optimizer,
+               layer_size=32,
+               initial_num_layers=0,
+               learn_mixture_weights=False,
+               dropout=0.,
+               seed=None):
+    """Initializes a DNN `Generator`.
+
+    Args:
+      feature_columns: An iterable containing all the feature columns used
+        by DNN models. All items in the set should be instances of classes
+        derived from `FeatureColumn`.
+      optimizer: An `Optimizer` instance for training both the base learner and
+        the mixture weights.
+      layer_size: Number of nodes in each hidden layer of the base learner
+        candidates. Note that this parameter is ignored in a DNN with no
+        hidden layers.
+      initial_num_layers: Minimum number of layers for each DNN base learner.
+        At iteration 0, the base learners will be `initial_num_layers` deep.
+        base learners at subsequent iterations will be at least as deep.
+      learn_mixture_weights: Whether to solve a learning problem to find the
+        best mixture weights, or use their default value according to the
+        mixture weight type. When `False`, the base learners will return a no_op
+        for the mixture weight train op.
+      dropout: The dropout rate, between 0 and 1. E.g. "rate=0.1" would drop
+        out 10% of input units.
+      seed: A random seed.
+
+    Returns:
+      An instance of `Generator`.
+
+    Raises:
+      ValueError: If feature_columns is empty.
+      ValueError: If layer_size < 1.
+      ValueError: If initial_num_layers < 0.
+    """
+
+    if not feature_columns:
+      raise ValueError("feature_columns must not be empty")
+
+    if layer_size < 1:
+      raise ValueError("layer_size must be >= 1")
+
+    if initial_num_layers < 0:
+      raise ValueError("initial_num_layers must be >= 0")
+
+    self._initial_num_layers = initial_num_layers
+    self._dnn_builder_fn = functools.partial(
+        _SimpleDNNBuilder,
+        feature_columns=feature_columns,
+        optimizer=optimizer,
+        layer_size=layer_size,
+        learn_mixture_weights=learn_mixture_weights,
+        dropout=dropout,
+        seed=seed)
+
+  def generate_candidates(self, previous_ensemble):
+    """See `adanet.BaseLearnerBuilderGenerator`."""
+
+    num_layers = self._initial_num_layers
+    if previous_ensemble:
+      num_layers = tf.contrib.util.constant_value(
+          previous_ensemble.weighted_base_learners[
+              -1].base_learner.persisted_tensors[_NUM_LAYERS_KEY])
+    return [
+        self._dnn_builder_fn(num_layers=num_layers),
+        self._dnn_builder_fn(num_layers=num_layers + 1),
+    ]
---
+++ adanet/examples/simple_dnn_test.py
@@ -0,0 +1,189 @@
+"""Tests for a simple dense neural network search space.
+
+Copyright 2018 The AdaNet Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import parameterized
+import adanet
+from adanet.examples import simple_dnn
+import tensorflow as tf
+
+
+class _FakeEnsemble(object):
+  """A fake ensemble of one base learner."""
+
+  def __init__(self, num_layers):
+    persisted_tensors = {"num_layers": tf.constant(num_layers)}
+    self._weighted_base_learners = [
+        adanet.WeightedBaseLearner(
+            weight=None,
+            logits=None,
+            base_learner=adanet.BaseLearner(
+                last_layer=[1],
+                logits=[1],
+                complexity=1,
+                persisted_tensors=persisted_tensors))
+    ]
+
+  @property
+  def weighted_base_learners(self):
+    return self._weighted_base_learners
+
+
+class GeneratorTest(parameterized.TestCase, tf.test.TestCase):
+
+  @parameterized.named_parameters({
+      "testcase_name": "defaults",
+      "want_names": ["linear", "1_layer_dnn"],
+      "want_base_learner_losses": [.871, .932],
+      "want_mixture_weight_losses": [.871, .932],
+      "want_complexities": [0., 1.],
+  }, {
+      "testcase_name": "learn_mixture_weights",
+      "learn_mixture_weights": True,
+      "want_names": ["linear", "1_layer_dnn"],
+      "want_base_learner_losses": [.871, .932],
+      "want_mixture_weight_losses": [.842, .892],
+      "want_complexities": [0., 1.],
+  }, {
+      "testcase_name": "one_initial_num_layers",
+      "initial_num_layers": 1,
+      "want_names": ["1_layer_dnn", "2_layer_dnn"],
+      "want_base_learner_losses": [.932, .660],
+      "want_mixture_weight_losses": [.932, .660],
+      "want_complexities": [1., 1.414],
+  }, {
+      "testcase_name": "previous_ensemble",
+      "previous_ensemble": _FakeEnsemble(1),
+      "want_names": ["1_layer_dnn", "2_layer_dnn"],
+      "want_base_learner_losses": [.932, .660],
+      "want_mixture_weight_losses": [.932, .660],
+      "want_complexities": [1., 1.414],
+  })
+  def test_generate_candidates(self,
+                               want_names,
+                               want_base_learner_losses,
+                               want_mixture_weight_losses,
+                               want_complexities,
+                               learn_mixture_weights=False,
+                               initial_num_layers=0,
+                               previous_ensemble=None):
+    feature_columns = [tf.feature_column.numeric_column("x")]
+    generator = simple_dnn.Generator(
+        feature_columns=feature_columns,
+        optimizer=tf.train.GradientDescentOptimizer(.1),
+        layer_size=3,
+        initial_num_layers=initial_num_layers,
+        learn_mixture_weights=learn_mixture_weights,
+        seed=42)
+    with tf.Graph().as_default() as g:
+      iteration_step = tf.train.create_global_step()
+      features = {"x": [[1.], [2.]]}
+      labels = tf.constant([[0.], [1.]])
+      names = []
+      base_learner_losses = []
+      mixture_weight_losses = []
+      complexities = []
+      for builder in generator.generate_candidates(previous_ensemble):
+        names.append(builder.name)
+
+        # 1. Build base learner graph.
+        base_learner = builder.build_base_learner(
+            features,
+            logits_dimension=1,
+            training=True,
+            iteration_step=iteration_step,
+            summary=tf.summary,
+            previous_ensemble=previous_ensemble)
+
+        # 2. Build base learner train ops.
+        base_learner_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=base_learner.logits, labels=labels))
+        base_learner_train_op = builder.build_base_learner_train_op(
+            base_learner_loss,
+            var_list=None,
+            labels=labels,
+            iteration_step=iteration_step,
+            summary=tf.summary)
+
+        # 3. Build mixture weight train ops.
+
+        # Stop gradients since mixture weights should have not propagate
+        # beyond top layer.
+        base_learner_logits = tf.stop_gradient(base_learner.logits)
+
+        # Mixture weight will initialize to a one-valued scalar.
+        mixture_weight_logits = tf.layers.dense(
+            base_learner_logits,
+            units=1,
+            use_bias=False,
+            kernel_initializer=tf.ones_initializer())
+        mixture_weight_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=mixture_weight_logits, labels=labels))
+        mixture_weight_train_op = builder.build_mixture_weights_train_op(
+            mixture_weight_loss,
+            var_list=None,
+            labels=labels,
+            logits=mixture_weight_logits,
+            iteration_step=iteration_step,
+            summary=tf.summary)
+
+        with self.test_session(graph=g) as sess:
+          sess.run(tf.global_variables_initializer())
+          sess.run(base_learner_train_op)
+          sess.run(mixture_weight_train_op)
+          base_learner_losses.append(sess.run(base_learner_loss))
+          mixture_weight_losses.append(sess.run(mixture_weight_loss))
+          complexities.append(sess.run(base_learner.complexity))
+
+    self.assertEqual(want_names, names)
+    self.assertAllClose(
+        want_base_learner_losses, base_learner_losses, atol=1e-3)
+    self.assertAllClose(
+        want_mixture_weight_losses, mixture_weight_losses, atol=1e-3)
+    self.assertAllClose(want_complexities, complexities, atol=1e-3)
+
+  @parameterized.named_parameters({
+      "testcase_name": "empty_feature_columns",
+      "feature_columns": [],
+  }, {
+      "testcase_name": "zero_layer_size",
+      "feature_columns": [tf.feature_column.numeric_column("x")],
+      "layer_size": 0,
+  }, {
+      "testcase_name": "negative_initial_num_layers",
+      "feature_columns": [tf.feature_column.numeric_column("x")],
+      "initial_num_layers": -1,
+  })
+  def test_constructor_errors(self,
+                              feature_columns,
+                              layer_size=3,
+                              initial_num_layers=0):
+    with self.assertRaises(ValueError):
+      simple_dnn.Generator(
+          feature_columns=feature_columns,
+          optimizer=tf.train.GradientDescentOptimizer(.1),
+          layer_size=layer_size,
+          initial_num_layers=initial_num_layers)
+
+
+if __name__ == "__main__":
+  tf.test.main()
