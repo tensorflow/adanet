Fix off-by-one error in iteration training steps.
Previously, an iteration would end one step too early. The fix was to pass the iteration step which is zero-indexed, instead of one-indexed, to the candidate. This had the additional benefit of removing a redundant `tf.Variable` that was duplicating the work of tracking the current iteration step.

Also eliminate non-determinism in reading the value of iteration step and global step. These are now incremented after all the train ops are complete.

PiperOrigin-RevId: 208482865

adanet/core/candidate.py
@@ -27,22 +27,16 @@ from tensorflow.python.training import moving_averages
 
 
 class _Candidate(
-    collections.namedtuple("_Candidate", [
-        "ensemble", "adanet_loss", "is_training", "update_op",
-        "is_previous_best"
-    ])):
+    collections.namedtuple(
+        "_Candidate",
+        ["ensemble", "adanet_loss", "is_training", "is_previous_best"])):
   """An AdaNet candidate.
 
   A `_Candidate` tracks the progress of a candidate base learner's training
   within an ensemble, as well as their AdaNet loss over time.
   """
 
-  def __new__(cls,
-              ensemble,
-              adanet_loss,
-              is_training,
-              update_op=None,
-              is_previous_best=False):
+  def __new__(cls, ensemble, adanet_loss, is_training, is_previous_best=False):
     """Creates a validated `_Candidate` instance.
 
     Args:
@@ -50,7 +44,6 @@ class _Candidate(
       adanet_loss: float `Tensor` representing the ensemble's AdaNet loss
         on the training set as defined in Equation (4) of the paper.
       is_training: bool `Tensor` indicating if training is ongoing.
-      update_op: Optional op for updating the candidate's variables.
       is_previous_best: bool identifying whether this ensemble came from the
         previous iteration.
 
@@ -67,14 +60,11 @@ class _Candidate(
       raise ValueError("adanet_loss is required")
     if is_training is None:
       raise ValueError("is_training is required")
-    if update_op is None:
-      update_op = tf.no_op()
     return super(_Candidate, cls).__new__(
         cls,
         ensemble=ensemble,
         adanet_loss=adanet_loss,
         is_training=is_training,
-        update_op=update_op,
         is_previous_best=is_previous_best)
 
 
@@ -103,19 +93,20 @@ class _CandidateBuilder(object):
     self._adanet_loss_decay = adanet_loss_decay
     super(_CandidateBuilder, self).__init__()
 
-  def build_candidate(self, ensemble, training, summary,
+  def build_candidate(self,
+                      ensemble,
+                      training,
+                      iteration_step,
+                      summary,
                       is_previous_best=False):
     """Builds and returns an AdaNet candidate.
-
-    When creating a candidate from a ensemble, it creates a train loss
-    to track its train loss over time. From this information, it compares its
-    performance against the previous best candidate, and determines whether
-    it should keep training.
 
     Args:
       ensemble: `Ensemble` instance to track.
       training: A python boolean indicating whether the graph is in training
         mode or prediction mode.
+      iteration_step: Integer `Tensor` representing the step since the
+        beginning of the current iteration, as opposed to the global step.
       summary: A `Summary` for recording summaries for TensorBoard.
       is_previous_best: Bool identifying whether this ensemble came from a
         previous iteration. If `True`, `is_training` will be `False` since its
@@ -131,26 +122,21 @@ class _CandidateBuilder(object):
       adanet_loss = tf.get_variable(
           "adanet_loss", initializer=0., trainable=False)
 
-      # Counter variable to track the number of steps.
-      step_counter = tf.get_variable(
-          "step_counter", initializer=1, trainable=False)
-
-      # Update train loss during training so that it's available in other modes.
-      update_op = tf.assign_add(step_counter, 1) if training else tf.no_op()
-
       if is_previous_best:
         # This candidate is frozen, so it is already done training.
         is_training = tf.constant(False, name="is_training")
       else:
         # Train this candidate for `max_steps` steps.
-        with tf.control_dependencies([update_op]):
-          is_training = tf.less(
-              step_counter.read_value(), self._max_steps, name="is_training")
+        # NOTE: During training, the iteration step gets incremented at the very
+        # end of the computation graph, so we need to account for that here.
+        is_training = tf.less(
+            iteration_step + 1 if training else 0,
+            self._max_steps,
+            name="is_training")
 
       if training:
-        with tf.control_dependencies([ensemble.adanet_loss]):
-          update_adanet_loss_op = moving_averages.assign_moving_average(
-              adanet_loss, ensemble.adanet_loss, decay=self._adanet_loss_decay)
+        update_adanet_loss_op = moving_averages.assign_moving_average(
+            adanet_loss, ensemble.adanet_loss, decay=self._adanet_loss_decay)
         with tf.control_dependencies([update_adanet_loss_op]):
           adanet_loss = adanet_loss.read_value()
 
@@ -164,5 +150,4 @@ class _CandidateBuilder(object):
           ensemble=ensemble,
           adanet_loss=adanet_loss,
           is_training=is_training,
-          update_op=update_op,
           is_previous_best=is_previous_best)

adanet/core/candidate_test.py
@@ -65,78 +65,68 @@ class CandidateTest(parameterized.TestCase, tf.test.TestCase):
 
 class CandidateBuilderTest(parameterized.TestCase, tf.test.TestCase):
 
-  @parameterized.named_parameters(
-      {
-          "testcase_name": "evaluate",
-          "training": False,
-          "max_steps": 3,
-          "want_adanet_losses": [0., 0., 0.],
-          "want_is_training": True,
-      },
-      {
-          "testcase_name": "train_exactly_max_steps",
-          "training": True,
-          "max_steps": 3,
-          "want_adanet_losses": [1., .750, .583],
-          "want_is_training": False,
-      },
-      {
-          "testcase_name": "train_extra_steps",
-          "training": True,
-          "max_steps": 2,
-          "want_adanet_losses": [1., .750, .583],
-          "want_is_training": False,
-      },
-      {
-          "testcase_name": "train_one_step_max_one_step",
-          "training": True,
-          "max_steps": 1,
-          "want_adanet_losses": [1.],
-          "want_is_training": False,
-      },
-      {
-          "testcase_name": "train_one_step_max_two_steps",
-          "training": True,
-          "max_steps": 2,
-          "want_adanet_losses": [1.],
-          "want_is_training": False,  # TODO. Should be `True`.
-      },
-      {
-          "testcase_name": "train_two_steps_max_two_steps",
-          "training": True,
-          "max_steps": 2,
-          "want_adanet_losses": [1., .750],
-          "want_is_training": False,
-      },
-      {
-          "testcase_name": "train_three_steps_max_four_steps",
-          "training": True,
-          "max_steps": 4,
-          "want_adanet_losses": [1., .750, .583],
-          "want_is_training": False,  # TODO. Should be `True`.
-      },
-      {
-          "testcase_name": "train_three_steps_max_five_steps",
-          "training": True,
-          "max_steps": 5,
-          "want_adanet_losses": [1., .750, .583],
-          "want_is_training": True,
-      },
-      {
-          "testcase_name": "eval_one_step",
-          "training": False,
-          "max_steps": 1,
-          "want_adanet_losses": [0.],
-          "want_is_training": False,  # TODO. Should be `True`.
-      },
-      {
-          "testcase_name": "previous_best_training",
-          "training": True,
-          "is_previous_best": True,
-          "max_steps": 4,
-          "want_adanet_losses": [1., .750, .583],
-          "want_is_training": False,
-      })
+  @parameterized.named_parameters({
+      "testcase_name": "evaluate",
+      "training": False,
+      "max_steps": 3,
+      "want_adanet_losses": [0., 0., 0.],
+      "want_is_training": True,
+  }, {
+      "testcase_name": "train_exactly_max_steps",
+      "training": True,
+      "max_steps": 3,
+      "want_adanet_losses": [1., .750, .583],
+      "want_is_training": False,
+  }, {
+      "testcase_name": "train_extra_steps",
+      "training": True,
+      "max_steps": 2,
+      "want_adanet_losses": [1., .750, .583],
+      "want_is_training": False,
+  }, {
+      "testcase_name": "train_one_step_max_one_step",
+      "training": True,
+      "max_steps": 1,
+      "want_adanet_losses": [1.],
+      "want_is_training": False,
+  }, {
+      "testcase_name": "train_one_step_max_two_steps",
+      "training": True,
+      "max_steps": 2,
+      "want_adanet_losses": [1.],
+      "want_is_training": True,
+  }, {
+      "testcase_name": "train_two_steps_max_two_steps",
+      "training": True,
+      "max_steps": 2,
+      "want_adanet_losses": [1., .750],
+      "want_is_training": False,
+  }, {
+      "testcase_name": "train_three_steps_max_four_steps",
+      "training": True,
+      "max_steps": 4,
+      "want_adanet_losses": [1., .750, .583],
+      "want_is_training": True,
+  }, {
+      "testcase_name": "train_three_steps_max_five_steps",
+      "training": True,
+      "max_steps": 5,
+      "want_adanet_losses": [1., .750, .583],
+      "want_is_training": True,
+  }, {
+      "testcase_name": "eval_one_step",
+      "training": False,
+      "max_steps": 1,
+      "want_adanet_losses": [0.],
+      "want_is_training": True,
+  }, {
+      "testcase_name": "previous_best_training",
+      "training": True,
+      "is_previous_best": True,
+      "max_steps": 4,
+      "want_adanet_losses": [1., .750, .583],
+      "want_is_training": False,
+  })
   def test_build_candidate(self,
                            training,
                            max_steps,
@@ -149,10 +139,12 @@ class CandidateBuilderTest(parameterized.TestCase, tf.test.TestCase):
     fake_ensemble = tu.dummy_ensemble(
         "new", adanet_loss=fake_adanet_loss, train_op=fake_train_op)
 
+    iteration_step = tf.Variable(0)
     builder = _CandidateBuilder(max_steps=max_steps)
     candidate = builder.build_candidate(
         ensemble=fake_ensemble,
         training=training,
+        iteration_step=iteration_step,
         summary=tf.summary,
         is_previous_best=is_previous_best)
     with self.test_session() as sess:
@@ -160,10 +152,10 @@ class CandidateBuilderTest(parameterized.TestCase, tf.test.TestCase):
       adanet_losses = []
       is_training = True
       for _ in range(len(want_adanet_losses)):
-        is_training, adanet_loss, _ = sess.run(
-            (candidate.is_training, candidate.adanet_loss, candidate.update_op))
+        is_training, adanet_loss = sess.run(
+            (candidate.is_training, candidate.adanet_loss))
         adanet_losses.append(adanet_loss)
-        sess.run(fake_train_op)
+        sess.run((fake_train_op, tf.assign_add(iteration_step, 1)))
 
     # Verify that adanet_loss moving average works.
     self.assertAllClose(want_adanet_losses, adanet_losses, atol=1e-3)

adanet/core/iteration.py
@@ -58,7 +58,8 @@ class _Iteration(
       is_over: Boolean `Tensor` indicating if iteration is over.
       base_learner_reports: Dict mapping string names to `BaseLearnerReport`s,
         one per candidate.
-      step: Integer `Tensor` indicating the current step of the iteration.
+      step: Integer `Tensor` representing the step since the beginning of the
+        current iteration, as opposed to the global step.
 
     Raises:
       ValueError: If validation fails.
@@ -149,6 +150,17 @@ class _IterationBuilder(object):
     training = mode == tf.estimator.ModeKeys.TRAIN
     skip_summaries = mode == tf.estimator.ModeKeys.PREDICT
     with tf.variable_scope("iteration_{}".format(iteration_number)):
+      # Iteration step to use instead of global step.
+      iteration_step = tf.get_variable(
+          "step",
+          shape=[],
+          initializer=tf.zeros_initializer(),
+          trainable=False,
+          dtype=tf.int64)
+
+      # Convert to tensor so that users cannot mutate it.
+      iteration_step_tensor = tf.convert_to_tensor(iteration_step)
+
       seen_builder_names = {}
       candidates = []
       summaries = []
@@ -163,6 +175,7 @@ class _IterationBuilder(object):
         previous_best_candidate = self._candidate_builder.build_candidate(
             ensemble=previous_ensemble,
             training=training,
+            iteration_step=iteration_step_tensor,
             summary=previous_ensemble_summary,
             is_previous_best=True)
         candidates.append(previous_best_candidate)
@@ -180,14 +193,6 @@ class _IterationBuilder(object):
               previous_ensemble.adanet_loss)
           base_learner_reports["previous_ensemble"] = base_learner_report
 
-      # Iteration step to use instead of global step.
-      iteration_step = tf.get_variable(
-          "step",
-          shape=[],
-          initializer=tf.zeros_initializer(),
-          trainable=False,
-          dtype=tf.int64)
-
       for base_learner_builder in base_learner_builders:
         if base_learner_builder.name in seen_builder_names:
           raise ValueError("Two ensembles have the same name '{}'".format(
@@ -202,11 +207,14 @@ class _IterationBuilder(object):
             summary=summary,
             features=features,
             mode=mode,
-            iteration_step=iteration_step,
+            iteration_step=iteration_step_tensor,
             labels=labels,
         )
         candidate = self._candidate_builder.build_candidate(
-            ensemble=ensemble, training=training, summary=summary)
+            ensemble=ensemble,
+            training=training,
+            iteration_step=iteration_step_tensor,
+            summary=summary)
         candidates.append(candidate)
 
         # Generate base learner reports.
@@ -253,7 +261,7 @@ class _IterationBuilder(object):
           summaries=summaries,
           is_over=self._is_over(candidates),
           base_learner_reports=base_learner_reports,
-          step=iteration_step.read_value())
+          step=iteration_step_tensor)
 
   def _is_over(self, candidates):
     """The iteration is over once all candidates are done training."""
@@ -289,19 +297,18 @@ class _IterationBuilder(object):
       return None
     with tf.variable_scope("train_op"):
       train_ops = []
-      step_ops = []
-      if mode == tf.estimator.ModeKeys.TRAIN:
-        # AdaNet is responsible for incrementing the global step, not the
-        # candidates it trains.
-        step_ops.append(tf.assign_add(tf.train.get_global_step(), 1))
-        step_ops.append(tf.assign_add(step, 1))
       for candidate in candidates:
-        train_ops.append(candidate.update_op)
         if candidate.ensemble.train_op is not None:
           # The train op of a previous ensemble is None even during `TRAIN`.
           train_ops.append(candidate.ensemble.train_op)
       with tf.control_dependencies(train_ops):
-        return tf.group(*step_ops)
+        # AdaNet is responsible for incrementing the global step, not the
+        # candidates it trains. Incrementing the global step and iteration step
+        # is the final action performed in the train op.
+        return tf.group(
+            tf.assign_add(tf.train.get_global_step(), 1),
+            tf.assign_add(step, 1),
+        )
 
   def _best_eval_metric_ops(self, candidates, best_candidate_index):
     """Returns the eval metric ops of the best candidate.

adanet/core/iteration_test.py
@@ -31,49 +31,56 @@ import adanet.core.testing_utils as tu
 import tensorflow as tf
 
 
+def _dummy_candidate():
+  """Returns a dummy `_Candidate` instance."""
+
+  return _Candidate(
+      ensemble=tu.dummy_ensemble("foo"), adanet_loss=1., is_training=True)
+
+
 class IterationTest(parameterized.TestCase, tf.test.TestCase):
 
   # pylint: disable=g-long-lambda
   @parameterized.named_parameters({
       "testcase_name": "single_candidate",
       "number": 0,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": True,
   }, {
       "testcase_name": "two_candidates",
       "number": 0,
-      "candidates": [tu.dummy_candidate(),
-                     tu.dummy_candidate()],
+      "candidates": [_dummy_candidate(),
+                     _dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": True,
   }, {
       "testcase_name": "positive_number",
       "number": 1,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": True,
   }, {
       "testcase_name": "false_is_over",
       "number": 1,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": False,
   }, {
       "testcase_name": "zero_best_predictions",
       "number": 1,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": True,
   }, {
       "testcase_name": "zero_best_loss",
       "number": 1,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec": tu.dummy_estimator_spec(),
       "best_candidate_index": 0,
       "is_over": True,
@@ -82,7 +89,7 @@ class IterationTest(parameterized.TestCase, tf.test.TestCase):
           "pass_base_learner_report",
       "number":
           1,
-      "candidates": [tu.dummy_candidate()],
+      "candidates": [_dummy_candidate()],
       "estimator_spec":
           tu.dummy_estimator_spec(),
       "best_candidate_index":
@@ -143,7 +150,7 @@ class IterationTest(parameterized.TestCase, tf.test.TestCase):
       "candidates": lambda: None,
   }, {
       "testcase_name": "non_list_candidates",
-      "candidates": lambda: {"foo": tu.dummy_candidate()},
+      "candidates": lambda: {"foo": _dummy_candidate()},
   }, {
       "testcase_name": "none_estimator_spec",
       "estimator_spec": None,
@@ -159,7 +166,7 @@ class IterationTest(parameterized.TestCase, tf.test.TestCase):
   })
   def test_new_errors(self,
                       number=0,
-                      candidates=lambda: [tu.dummy_candidate()],
+                      candidates=lambda: [_dummy_candidate()],
                       estimator_spec=tu.dummy_estimator_spec(),
                       best_candidate_index=0,
                       is_over=True,
@@ -318,18 +325,19 @@ class _FakeCandidateBuilder(object):
   def build_candidate(self,
                       ensemble,
                       training,
+                      iteration_step,
                       summary,
                       previous_ensemble=None,
                       is_previous_best=False):
     del training  # Unused
+    del iteration_step  # Unused
     del summary  # Unused
     del previous_ensemble  # Unused
     return _Candidate(
         ensemble=ensemble,
         adanet_loss=ensemble.adanet_loss,
         is_training="training" in ensemble.name,
-        is_previous_best=is_previous_best,
-        update_op=ensemble.train_op)
+        is_previous_best=is_previous_best)
 
 
 def _metric_ops_a():