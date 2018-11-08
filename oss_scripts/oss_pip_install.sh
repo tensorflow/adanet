6abde92ce33aaed0c59f6f581d17cea268cf2eb1
Charles Weill <weill@google.com>
2018-11-07 23:17:54 -0500

    proper install

--- oss_scripts/oss_pip_install.sh
+++ oss_scripts/oss_pip_install.sh
@@ -19,6 +19,17 @@ pip install -q -U numpy
 # First ensure that the base dependencies are sufficient for a full import
 # pip install -q -e .
 
+# Install Bazel for tests.
+# Step 1: Install the JDK
+sudo apt-get install openjdk-8-jdk
+
+# Step 2: Add Bazel distribution URI as a package source
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+# Step 3: Install and update Bazel
+sudo apt-get update && sudo apt-get install bazel
+
 # Build adanet pip packaging script
 bazel build //adanet/pip_package:build_pip_package
 
@@ -30,8 +41,6 @@ pip install /tmp/adanet_pkg/*.whl
 
 python -c "import adanet"
 
-# Install Bazel for tests.
-sudo apt-get install openjdk-8-jdk bazel
 
 
 # Then install the test dependencies
