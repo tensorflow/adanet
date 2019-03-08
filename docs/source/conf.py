26ecf359094b1e772b9b842a4a7019b84a2731fb
Lorenz Nickel <mail@lorenznickel.de>
2019-03-08 13:14:59 +0100

    Updated link (redirect)
    Link redirected to this page, updated it here

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 """Configuration file for the Sphinx documentation builder.
 
 This file does only contain a selection of the most common options. For a
-full list see the documentation: http://www.sphinx-doc.org/en/master/config
+full list see the documentation: http://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
 # -- Path setup --------------------------------------------------------------
