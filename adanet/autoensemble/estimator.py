Fix collocation error when using multiclass gradient boosted decision trees in AutoEnsembleEstimator.
PiperOrigin-RevId: 248018024

adanet/autoensemble/estimator.py
@@ -98,16 +98,21 @@ class _GeneratorFromCandidatePool(Generator):
     self._logits_fn = logits_fn
 
   def generate_candidates(self, previous_ensemble, iteration_number,
-                          previous_ensemble_reports, all_reports):
+                          previous_ensemble_reports, all_reports, config):
+    assert config
     builders = []
-    if isinstance(self._candidate_pool, dict):
-      for name in sorted(self._candidate_pool):
+    candidate_pool = self._candidate_pool
+    if callable(candidate_pool):
+      # candidate_pool can be a function.
+      candidate_pool = candidate_pool(config=config)
+    if isinstance(candidate_pool, dict):
+      for name in sorted(candidate_pool):
         builders.append(
             _BuilderFromEstimator(
-                name, self._candidate_pool[name], logits_fn=self._logits_fn))
+                name, candidate_pool[name], logits_fn=self._logits_fn))
       return builders
 
-    for i, estimator in enumerate(self._candidate_pool):
+    for i, estimator in enumerate(candidate_pool):
       name = "{class_name}{index}".format(
           class_name=estimator.__class__.__name__, index=i)
       builders.append(
@@ -137,16 +142,18 @@ class AutoEnsembleEstimator(Estimator):
       # Learn to ensemble linear and DNN models.
       estimator = adanet.AutoEnsembleEstimator(
           head=head,
-          candidate_pool={
+          candidate_pool=lambda config: {
               "linear":
                   tf.estimator.LinearEstimator(
                       head=head,
                       feature_columns=feature_columns,
+                      config=config,
                       optimizer=...),
               "dnn":
                   tf.estimator.DNNEstimator(
                       head=head,
                       feature_columns=feature_columns,
+                      config=config,
                       optimizer=...,
                       hidden_units=[1000, 500, 100])},
           max_iteration_steps=50)
@@ -176,6 +183,9 @@ class AutoEnsembleEstimator(Estimator):
       candidates will be included in the final ensemble, but will affect the
       name of the candidate. When using a dict, the string key will be used as
       the name of the candidate.
+      Alternatively, this argument can be a function that takes a `config`
+      argument and returns the aforementioned values in case the
+      :code:`Estimators` need to be instantiated at each adanet iteration.
     max_iteration_steps: Total number of steps for which to train candidates per
       iteration. If `OutOfRange` or `StopIteration` occurs in the middle,
       training stops before `max_iteration_steps` steps.

adanet/autoensemble/estimator_test.py
@@ -23,7 +23,6 @@ import os
 import shutil
 
 from absl.testing import parameterized
-from adanet import tf_compat
 from adanet.autoensemble.estimator import AutoEnsembleEstimator
 import tensorflow as tf
 
@@ -35,23 +34,78 @@ tf.logging.set_verbosity(tf.logging.INFO)
 class AutoEnsembleEstimatorTest(parameterized.TestCase, tf.test.TestCase):
 
   def setUp(self):
+    super(AutoEnsembleEstimatorTest, self).setUp()
     # Setup and cleanup test directory.
     self.test_subdirectory = os.path.join(tf.flags.FLAGS.test_tmpdir, self.id())
     shutil.rmtree(self.test_subdirectory, ignore_errors=True)
     os.makedirs(self.test_subdirectory)
 
   def tearDown(self):
+    super(AutoEnsembleEstimatorTest, self).tearDown()
     shutil.rmtree(self.test_subdirectory, ignore_errors=True)
 
+  # pylint: disable=g-long-lambda
   @parameterized.named_parameters(
       {
-          "testcase_name": "dict_candidate_pool",
-          "list_candidate_pool": False,
+          "testcase_name":
+              "dict_candidate_pool",
+          "candidate_pool":
+              lambda head, feature_columns, optimizer: {
+                  "dnn":
+                      tf.estimator.DNNEstimator(
+                          head=head,
+                          feature_columns=feature_columns,
+                          optimizer=optimizer,
+                          hidden_units=[3]),
+                  "linear":
+                      tf.estimator.LinearEstimator(
+                          head=head,
+                          feature_columns=feature_columns,
+                          optimizer=optimizer),
+              },
+          "want_loss":
+              .209,
       }, {
-          "testcase_name": "list_candidate_pool",
-          "list_candidate_pool": True,
+          "testcase_name":
+              "list_candidate_pool",
+          "candidate_pool":
+              lambda head, feature_columns, optimizer: [
+                  tf.estimator.DNNEstimator(
+                      head=head,
+                      feature_columns=feature_columns,
+                      optimizer=optimizer,
+                      hidden_units=[3]),
+                  tf.estimator.LinearEstimator(
+                      head=head,
+                      feature_columns=feature_columns,
+                      optimizer=optimizer),
+              ],
+          "want_loss":
+              .209,
+      }, {
+          "testcase_name":
+              "candidate_pool_lambda",
+          "candidate_pool":
+              lambda head, feature_columns, optimizer: lambda config: {
+                  "dnn":
+                      tf.estimator.DNNEstimator(
+                          head=head,
+                          feature_columns=feature_columns,
+                          optimizer=optimizer,
+                          hidden_units=[3],
+                          config=config),
+                  "linear":
+                      tf.estimator.LinearEstimator(
+                          head=head,
+                          feature_columns=feature_columns,
+                          optimizer=optimizer,
+                          config=config),
+              },
+          "want_loss":
+              .209,
       })
-  def test_auto_ensemble_estimator_lifecycle(self, list_candidate_pool):
+  # pylint: enable=g-long-lambda
+  def test_auto_ensemble_estimator_lifecycle(self, candidate_pool, want_loss):
     features = {"input_1": [[1., 0.]]}
     labels = [[1.]]
 
@@ -75,33 +129,9 @@ class AutoEnsembleEstimatorTest(parameterized.TestCase, tf.test.TestCase):
       ]).make_one_shot_iterator().get_next()
       return {"input_1": input_features}, None
 
-    if hasattr(tf.estimator, "LinearEstimator"):
-      linear_estimator_fn = tf.estimator.LinearEstimator
-    else:
-      linear_estimator_fn = tf.contrib.estimator.LinearEstimator
-    if hasattr(tf.estimator, "DNNEstimator"):
-      dnn_estimator_fn = tf.estimator.DNNEstimator
-    else:
-      dnn_estimator_fn = tf.contrib.estimator.DNNEstimator
-
-    candidate_pool = {
-        "linear":
-            linear_estimator_fn(
-                head=head, feature_columns=feature_columns,
-                optimizer=optimizer),
-        "dnn":
-            dnn_estimator_fn(
-                head=head,
-                feature_columns=feature_columns,
-                optimizer=optimizer,
-                hidden_units=[3])
-    }
-    if list_candidate_pool:
-      candidate_pool = [candidate_pool[k] for k in sorted(candidate_pool)]
-
     estimator = AutoEnsembleEstimator(
         head=head,
-        candidate_pool=candidate_pool,
+        candidate_pool=candidate_pool(head, feature_columns, optimizer),
         max_iteration_steps=10,
         force_grow=True,
         model_dir=self.test_subdirectory,
@@ -113,12 +143,7 @@ class AutoEnsembleEstimatorTest(parameterized.TestCase, tf.test.TestCase):
     # Evaluate.
     eval_results = estimator.evaluate(input_fn=train_input_fn, steps=1)
 
-    want_loss = .209
-    if tf_compat.version_greater_or_equal("1.10.0") and (
-        not tf_compat.version_greater_or_equal("1.12.0")):
-      # Only TF 1.10 and 1.11.
-      want_loss = .079514
-    self.assertAllClose(want_loss, eval_results["loss"], atol=.05)
+    self.assertAllClose(want_loss, eval_results["loss"], atol=.2)
 
     # Predict.
     predictions = estimator.predict(input_fn=test_input_fn)

adanet/core/estimator.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 
 import contextlib
 import errno
+import inspect
 import json
 import os
 import time
@@ -1160,7 +1161,7 @@ class Estimator(tf.estimator.Estimator):
   # TODO: Refactor architecture building logic to its own module.
   def _architecture_ensemble_spec(self, architecture, iteration_number,
                                   features, mode, labels,
-                                  previous_ensemble_spec):
+                                  previous_ensemble_spec, config):
     """Returns an `_EnsembleSpec` with the given architecture.
 
     Creates the ensemble architecture by calling `generate_subnetworks` on
@@ -1178,6 +1179,7 @@ class Estimator(tf.estimator.Estimator):
         (for multi-head). Can be `None`.
       previous_ensemble_spec: The `_EnsembleSpec` for the previous iteration.
         Will be `None` for the first iteration.
+      config: The current `tf.estimator.RunConfig`.
 
     Returns:
       An `EnsembleSpec` instance for the given architecture.
@@ -1199,11 +1201,12 @@ class Estimator(tf.estimator.Estimator):
         previous_ensemble_reports, all_reports = (
             self._collate_subnetwork_reports(iteration_number))
       generated_subnetwork_builders = (
-          self._subnetwork_generator.generate_candidates(
+          self._call_generate_candidates(
               previous_ensemble=previous_ensemble,
               iteration_number=iteration_number,
               previous_ensemble_reports=previous_ensemble_reports,
-              all_reports=all_reports))
+              all_reports=all_reports,
+              config=config))
       subnetwork_builder_names = {
           b.name: b for b in generated_subnetwork_builders
       }
@@ -1335,7 +1338,20 @@ class Estimator(tf.estimator.Estimator):
         scaffold=tf_compat.v1.train.Scaffold(summary_op=tf.constant("")),
         export_outputs=iteration_estimator_spec.export_outputs)
 
-  def _adanet_model_fn(self, features, labels, mode, params):
+  def _call_generate_candidates(self, previous_ensemble, iteration_number,
+                                previous_ensemble_reports, all_reports, config):
+    defined_args = inspect.getargspec(
+        self._subnetwork_generator.generate_candidates).args
+    generate_args = dict(
+        previous_ensemble=previous_ensemble,
+        iteration_number=iteration_number,
+        previous_ensemble_reports=previous_ensemble_reports,
+        all_reports=all_reports)
+    if "config" in defined_args:
+      generate_args["config"] = config
+    return self._subnetwork_generator.generate_candidates(**generate_args)
+
+  def _adanet_model_fn(self, features, labels, mode, params, config):
     """AdaNet model_fn.
 
     This model_fn is called at least three times per iteration:
@@ -1359,6 +1375,7 @@ class Estimator(tf.estimator.Estimator):
       mode: Defines whether this is training, evaluation or prediction. See
         `ModeKeys`.
       params: A dict of parameters.
+      config: The current `tf.estimator.RunConfig`.
 
     Returns:
       A `EstimatorSpec` instance.
@@ -1411,7 +1428,8 @@ class Estimator(tf.estimator.Estimator):
                     for t, n in architecture.subnetworks_grouped_by_iteration
                 ])))
         previous_ensemble_spec = self._architecture_ensemble_spec(
-            architecture, i, features, mode, labels, previous_ensemble_spec)
+            architecture, i, features, mode, labels, previous_ensemble_spec,
+            config)
         previous_ensemble = previous_ensemble_spec.ensemble
         previous_ensemble_summary = self._summary_maker(
             namespace="ensemble",
@@ -1436,11 +1454,13 @@ class Estimator(tf.estimator.Estimator):
       if self._report_materializer:
         previous_ensemble_reports, all_reports = (
             self._collate_subnetwork_reports(iteration_number))
-      subnetwork_builders = self._subnetwork_generator.generate_candidates(
+
+      subnetwork_builders = self._call_generate_candidates(
           previous_ensemble=previous_ensemble,
           iteration_number=iteration_number,
           previous_ensemble_reports=previous_ensemble_reports,
-          all_reports=all_reports)
+          all_reports=all_reports,
+          config=config)
       ensemble_candidates = []
       for ensemble_strategy in self._ensemble_strategies:
         ensemble_candidates += ensemble_strategy.generate_ensemble_candidates(

adanet/core/estimator_distributed_test_runner.py
@@ -46,7 +46,14 @@ from adanet.subnetwork import Subnetwork
 import tensorflow as tf
 
 # pylint: disable=g-direct-tensorflow-import
+
+# Contrib
+from tensorflow.contrib.boosted_trees.estimator_batch.estimator import CoreGradientBoostedDecisionTreeEstimator
+from tensorflow.contrib.boosted_trees.proto import learner_pb2
+from tensorflow.contrib.boosted_trees.python.utils import losses as bt_losses
+
 from tensorflow.python.training import session_manager as session_manager_lib
+from tensorflow_estimator.python.estimator.head import multi_class_head
 from tensorflow_estimator.python.estimator.head import regression_head
 
 # Module path changed. Try importing from new and old location to maintain
@@ -61,6 +68,7 @@ except ImportError:
 flags.DEFINE_enum("estimator_type", "estimator", [
     "estimator",
     "autoensemble",
+    "autoensemble_trees_multiclass",
 ], "The estimator type to train.")
 
 flags.DEFINE_enum("placement_strategy", "replication", [
@@ -193,8 +201,6 @@ def train_and_evaluate_estimator():
           # Ignore other workers; only talk to parameter servers.
           # Otherwise, when a chief/worker terminates, the others will hang.
           device_filters=["/job:ps"]))
-  head = regression_head.RegressionHead(
-      loss_reduction=tf_compat.SUM_OVER_BATCH_SIZE)
 
   kwargs = {
       "max_iteration_steps": 100,
@@ -206,6 +212,10 @@ def train_and_evaluate_estimator():
       "worker_wait_timeout_secs": 60,
       "config": config
   }
+  head = regression_head.RegressionHead(
+      loss_reduction=tf_compat.SUM_OVER_BATCH_SIZE)
+  features = [[1., 0.], [0., 0], [0., 1.], [1., 1.]]
+  labels = [[1.], [0.], [1.], [0.]]
   if FLAGS.placement_strategy == "round_robin":
     kwargs["experimental_placement_strategy"] = RoundRobinStrategy()
   if FLAGS.estimator_type == "autoensemble":
@@ -232,7 +242,6 @@ def train_and_evaluate_estimator():
 
     estimator = AutoEnsembleEstimator(
         head=head, candidate_pool=candidate_pool, **kwargs)
-
   elif FLAGS.estimator_type == "estimator":
     subnetwork_generator = SimpleGenerator([
         _DNNBuilder("dnn1", config, layer_size=3),
@@ -242,12 +251,46 @@ def train_and_evaluate_estimator():
 
     estimator = Estimator(
         head=head, subnetwork_generator=subnetwork_generator, **kwargs)
+  elif FLAGS.estimator_type == "autoensemble_trees_multiclass":
+    n_classes = 3
+    head = multi_class_head.MultiClassHead(
+        n_classes=n_classes, loss_reduction=tf_compat.SUM_OVER_BATCH_SIZE)
+
+    def tree_loss_fn(labels, logits):
+      result = bt_losses.per_example_maxent_loss(
+          labels=labels, logits=logits, num_classes=n_classes, weights=None)
+      return result[0]
+
+    tree_head = multi_class_head.MultiClassHead(
+        loss_fn=tree_loss_fn,
+        n_classes=n_classes,
+        loss_reduction=tf_compat.SUM_OVER_BATCH_SIZE)
+    labels = [[1], [0], [1], [2]]
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[2])]
+    candidate_pool = lambda config: {  # pylint: disable=g-long-lambda
+        "linear":
+            tf.estimator.LinearEstimator(
+                head=head,
+                feature_columns=feature_columns,
+                optimizer=tf.keras.optimizers.Adam(lr=.001),
+                config=config),
+        "gbdt":
+            CoreGradientBoostedDecisionTreeEstimator(
+                head=tree_head,
+                learner_config=learner_pb2.LearnerConfig(num_classes=n_classes),
+                examples_per_layer=8,
+                num_trees=None,
+                center_bias=False,  # Required for multi-class.
+                feature_columns=feature_columns,
+                config=config),
+    }
+
+    estimator = AutoEnsembleEstimator(
+        head=head, candidate_pool=candidate_pool, **kwargs)
 
   def input_fn():
-    xor_features = [[1., 0.], [0., 0], [0., 1.], [1., 1.]]
-    xor_labels = [[1.], [0.], [1.], [0.]]
-    input_features = {"x": tf.constant(xor_features, name="x")}
-    input_labels = tf.constant(xor_labels, name="y")
+    input_features = {"x": tf.constant(features, name="x")}
+    input_labels = tf.constant(labels, name="y")
     return input_features, input_labels
 
   train_hooks = [

adanet/subnetwork/generator.py
@@ -274,7 +274,7 @@ class Generator(object):
 
   @abc.abstractmethod
   def generate_candidates(self, previous_ensemble, iteration_number,
-                          previous_ensemble_reports, all_reports):
+                          previous_ensemble_reports, all_reports, config):
     # pyformat: disable
     """Generates :class:`adanet.subnetwork.Builder` instances for an iteration.
 
@@ -304,6 +304,8 @@ class Generator(object):
         List containing all the :class:`adanet.subnetwork.MaterializedReport`
         instances in an AdaNet iteration, starting from iteration 0, and
         ending at iteration t-1.
+      config: The current :class:`tf.estimator.RunConfig` object to configure
+        the runtime settings.
 
     Returns:
       A list of :class:`adanet.subnetwork.Builder` instances.